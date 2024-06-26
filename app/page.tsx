0e93036c215d24e65abd0a8e0778f05848bc9736
Bayomi Aremo <8955yomi@gmail.com>
2024-06-26 09:22:02 -0700

    done

--- app/page.tsx
+++ app/page.tsx
@@ -3,11 +3,11 @@ import Link from "next/link";
 // import Image from "next/image";
 export default function Home() {
   return (
-    <main className="flex flex-col gap-5 sm:w-[40%] w-[60%] bg-sky-400 p-5 rounded-md text-white h-[100vh] justify-center text-center">
-     <h1 className="text-4xl my-10">😎QUOTE GENERATOR 😎</h1>
+    <main className="flex flex-col gap-5 sm:w-[60%] w-[100%] bg-sky-400 p-5 rounded-md text-white h-[100vh] justify-center text-center">
+     <h1 className=" text-2xl sm:text-4xl my-10 font-bold">QUOTE GENERATOR😎</h1>
      <div className="flex gap-4 w-full justify-center items-center">
-     <Link href="/street" className="px-6 py-2 rounded-md bg-sky-600 hover:bg-sky-800 text-white h-12 text-center my-auto">Street Quotes</Link>
-     <Link href="/quotes" className="px-6 py-2 rounded-md bg-sky-600 hover:bg-sky-800 text-white h-12 text-center my-auto">Normal Quotes</Link>
+     <Link href="/street" className="px-6 py-2 rounded-md bg-sky-600 hover:bg-sky-800 text-white text-center my-auto text-sm sm:text-xl">Street Quotes</Link>
+     <Link href="/quotes" className="px-6 py-2 rounded-md bg-sky-600 hover:bg-sky-800 text-white text-center my-auto text-sm sm:text-xl">Normal Quotes</Link>
      </div>
     </main>
   );
